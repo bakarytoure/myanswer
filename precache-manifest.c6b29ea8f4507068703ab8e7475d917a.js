131d6de0c5ca5913aaa0f2ab91240d2da37cc0fe
bakarytoure <bakarytoure57@gmail.com>
2020-08-30 20:43:01 +0000

    Updates

---
+++ precache-manifest.c6b29ea8f4507068703ab8e7475d917a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff5835fd961549f9cac4cab7d4083bf",
+    "url": "/myanswer/index.html"
+  },
+  {
+    "revision": "a70469e82567190841e9",
+    "url": "/myanswer/static/css/main.ce5c3149.chunk.css"
+  },
+  {
+    "revision": "7ae240f679255bf95b7b",
+    "url": "/myanswer/static/js/2.71fa0b9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myanswer/static/js/2.71fa0b9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a70469e82567190841e9",
+    "url": "/myanswer/static/js/main.3141dec4.chunk.js"
+  },
+  {
+    "revision": "f782e4411681c7e14da1",
+    "url": "/myanswer/static/js/runtime-main.4f11b56f.js"
+  }
+]);
